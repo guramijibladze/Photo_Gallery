bb98ddf942b347f8187389bc1e87158bb2d78757
guramijibladze <guramijibladze@gmail.com>
2020-09-01 13:01:20 +0400

    up

--- firegram/src/comps/ProgressBar.js
+++ firegram/src/comps/ProgressBar.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import  { useEffect } from 'react';
 import useStorage from '../hooks/useStorage'; 
 
 const ProgressBar = ({file, setFile}) =>{
     const { url, progress } = useStorage(file);
 
-    // useEffect(() => {
-    //     if (url) {
-    //       setFile(null);
-    //     }
-    //   }, [url, setFile])
+    useEffect(() => {
+        if (url) {
+          setFile(null);
+        }
+      }, [url, setFile])
 
     return (
         <div className="progress-bar" style={{ width: progress + '%' }}>progress</div>
--- firegram/src/hooks/useStorage.js
+++ firegram/src/hooks/useStorage.js
@@ -17,8 +17,8 @@ const useStorage = (file) => {
             setError(err);
         }, async() =>{
             const url = await storageRef.getDownloadURL();
-            const createAt = timestamp;
-            collectionRef.add({ url, createAt});
+            const createdAt = timestamp();
+            await collectionRef.add({ url, createdAt });
             setUrl(url)
         })
     }, [file]);
